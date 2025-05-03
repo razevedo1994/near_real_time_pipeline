07849c262b8f6f82c43335cf363b6c1a2b31c45a
Rodrigo <razevedo.contato@gmail.com>
2025-05-02 22:13:01 -0300

    Add resources to define policies

--- s3_tables.tf
+++ s3_tables.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_s3tables_table_bucket" "s3tables_bucket" {
   name = "nrt-bucket"
 }
@@ -12,4 +14,39 @@ resource "aws_s3tables_table" "s3tables_table" {
   namespace        = aws_s3tables_namespace.s3tables_namespace.namespace
   table_bucket_arn = aws_s3tables_namespace.s3tables_bucket.table_bucket_arn
   format           = "ICEBERG"
+}
+
+# Policies
+
+data "aws_iam_policy_document" "table_bucket_policy" {
+  statement {
+    actions = ["s3tables:*"]
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_caller_identity.current.account_id]
+    }
+    resources = ["${aws_s3tables_table_bucket.s3tables_bucket.arn}/*"]
+  }
+}
+
+resource "aws_s3tables_table_bucket_policy" "table_bucket_policy" {
+  resource_policy  = data.aws_iam_policy_document.table_bucket_policy.json
+  table_bucket_arn = aws_s3tables_table_bucket.s3tables_bucket.arn
+}
+
+resource "aws_s3tables_table_policy" "s3table_policy" {
+  resource_policy  = data.aws_iam_policy_document.table_policy.json
+  name             = aws_s3tables_table.s3tables_table.name
+  namespace        = aws_s3tables_table.s3tables_table.namespace
+  table_bucket_arn = aws_s3tables_table.s3tables_table.table_bucket_arn
+}
+data "aws_iam_policy_document" "table_policy" {
+  statement {
+    actions = ["s3tables:*"]
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_caller_identity.current.account_id]
+    }
+    resources = ["${aws_s3tables_table.s3tables_table.arn}"]
+  }
 }
